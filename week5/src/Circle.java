e1b5b9902c9e5dac75d2cbd344a5f736161c8d24
nurankamisli <kamislinuran4@gmail.com>
2025-03-21 12:10:54 +0300

    week5-nurankamisli-1

---
+++ week5/src/Circle.java
@@ -0,0 +1,23 @@
+public class Circle {
+    private int radius;
+    private Point center;
+    public Circle(Point center, int radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+    public Point getCenter() {
+        return center;
+    }
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+    public int getRadius() {
+        return radius;
+
+    }
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+
+}
